84ecc24068106d2f44fcc75a7c8e969efc6668fd
Michi-gi <michi.gm@gmail.com>
2023-01-10 22:01:40 +0900

    add StationId and TableId in TableReturn

--- table.go
+++ table.go
@@ -1,62 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
-	"github.com/go-chi/chi"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/go-chi/chi"
 )
 
-
 type HourData struct {
-	Hour	string	`json:"hour"`
-	MinTimeTable	[]struct {
-		Minute	string	`json:"minute"`
-		KindId	string	`json:"kindId"`
-		DestinationId	string	`json:"destinationId"`
-		TrainId	string	`json:"trainId"`
-		TrainName	string	`json:"trainName"`
-		FirstStation	string	`json:"firstStation"`
-		Extra	string	`json:"extraTrain"`
-		VendorTrainId	string	`json:"vendorTrainId"`
-	}	`json:"minTimeTable"`
+	Hour         string `json:"hour"`
+	MinTimeTable []struct {
+		Minute        string `json:"minute"`
+		KindId        string `json:"kindId"`
+		DestinationId string `json:"destinationId"`
+		TrainId       string `json:"trainId"`
+		TrainName     string `json:"trainName"`
+		FirstStation  string `json:"firstStation"`
+		Extra         string `json:"extraTrain"`
+		VendorTrainId string `json:"vendorTrainId"`
+	} `json:"minTimeTable"`
 }
 
 type Table struct {
-	Props	struct {
-		PageProps	struct {
+	Props struct {
+		PageProps struct {
 			TimetableItem struct {
 				Master struct {
-					Kind	[]IdName	`json:"kind"`
-					Destination	[]IdName	`json:"destination"`
-				}	`json:"master"`
-				DirectionName	string	`json:"directionName"`
-				RailName	string	`json:"railName"`
-				HourTimeTables	[]HourData	`json:"hourTimeTable"`
-			}	`json:"timetableItem"`
-		}	`json:"PageProps"`
-	}	`json:"props"`
+					Kind        []IdName `json:"kind"`
+					Destination []IdName `json:"destination"`
+				} `json:"master"`
+				DirectionName  string     `json:"directionName"`
+				RailName       string     `json:"railName"`
+				HourTimeTables []HourData `json:"hourTimeTable"`
+			} `json:"timetableItem"`
+		} `json:"PageProps"`
+	} `json:"props"`
 }
 
 type TrainInTable struct {
-	Time	string	`json:"time"`
-	Kind	string	`json:"kind"`
-	Dest	string	`json:"destination"`
-	Id	string	`json:"id"`
-	Name	string	`json:"name"`
-	IsFirstStation	bool	`json:"isFirstStation"`
-	IsExtra	bool	`json:"isExtra"`
-	VendorNumber	string	`json:"vendorNumber"`
+	Time           string `json:"time"`
+	Kind           string `json:"kind"`
+	Dest           string `json:"destination"`
+	Id             string `json:"id"`
+	Name           string `json:"name"`
+	IsFirstStation bool   `json:"isFirstStation"`
+	IsExtra        bool   `json:"isExtra"`
+	VendorNumber   string `json:"vendorNumber"`
 }
 
 type TableReturn struct {
-	Direction	string	`json:"direction"`
-	LineName	string	`json:"lineName"`
-	Table	[]TrainInTable	`json:"table"`
-	DayOfWeekMap	map[string]string	`json:"dayOfWeekMap"`
+	StationId    string            `json:"stationId"`
+	TableId      string            `json:"tableId"`
+	Direction    string            `json:"direction"`
+	LineName     string            `json:"lineName"`
+	Table        []TrainInTable    `json:"table"`
+	DayOfWeekMap map[string]string `json:"dayOfWeekMap"`
 }
 
 func TableController(w http.ResponseWriter, r *http.Request) {
@@ -80,7 +81,7 @@ func TableController(w http.ResponseWriter, r *http.Request) {
 		if linkEle.Length() == 0 {
 			linkEle = s.Find("a")
 			kindref, _ := linkEle.Attr("href")
-			kindNum = kindref[len(kindref) - 1:]
+			kindNum = kindref[len(kindref)-1:]
 		} else if len(kind) > 0 {
 			kindNum = kind[0]
 		} else {
@@ -127,7 +128,7 @@ func TableController(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	ret := TableReturn{Direction: direction, LineName: line, Table: trains, DayOfWeekMap: dayOfWeekMap}
+	ret := TableReturn{StationId: stationId, TableId: tableId, Direction: direction, LineName: line, Table: trains, DayOfWeekMap: dayOfWeekMap}
 
 	body, _ := json.Marshal(ret)
 	w.Write(body)
